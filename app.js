c34687ffa596fe8ab161362f543825d8831d2a45
nibrazrehaman <nibrazrehaman18@gmail.com>
2023-02-23 06:55:52 +0530

    first hosted website

---
+++ app.js
@@ -0,0 +1,61 @@
+// let a,b,c;
+// a=5;
+// b=6;
+// c=a+b;
+// console.log(c);//to write in console
+// document.write(c);//to write in webpage
+
+
+// keywords in JS
+// ------------------
+// --var : no need of mentioning the datatype
+//   var score,L;
+//   score=false;
+//   L=typeof score; : tells the datatype of variable
+//   console.log(L);
+// --let
+// --const
+// --if (condition) {
+    
+// }
+// --switch
+// --function:'function'=declare;
+// --return
+// --try
+
+
+//'new' operator is used to declare array
+// var myArray=new Array(3);
+// myArray[0]="abc";
+// console.log(myArray);
+
+// switch example
+// let day;
+// day=4;
+// switch (day) {
+//     case 0:
+//         day="sunday";
+//         break;
+//     case 1:
+//         day="monday";
+//         break;
+//     case 2:
+//         day="tuesday";
+//         break;
+//     case 3:
+//         day="wednesday";
+//         break;
+//     case 4:
+//         day="thursday";
+//         break;
+//     case 5:
+//         day="friday";
+//         break;
+
+//     default:
+//         day="saturday";
+//         break;
+// }
+// console.log(day);
+
+
---
+++ index1.js
@@ -0,0 +1,74 @@
+// var x= document.getElementById("heading3");
+// var y= document.getElementById("p1");
+
+// // console.log(x);
+// // console.log(x.innerHTML);
+
+// // x.innerHTML="Apples";
+// y.innerHTML="this is changed via js";
+
+
+// var health=90;
+//  if(health>80){
+//     heading3.style = 'color:green'
+//     heading3.style = 'font-family:"Georgia'
+//  }else{
+//     p1.innerHTML="color:green"
+//  }
+
+
+// var b=document.getElementById('btn');
+// var health=0;
+// function abc(){
+//     health++;
+//     y.innerHTML=health;
+//     y.style='color:green';
+
+    
+// }
+
+// b.addEventListener('click',abc);
+
+
+// OOPS
+
+// class user{
+//     constructor(first,last,age){
+//         this.first=first;
+//         this.last=last;
+//         this.age=age;
+//     }
+//     getFullName(){
+//         console.log(this.first+" "+this.last)
+//     }
+   
+
+//     reverse(){
+//         var temp=this.last;
+//         this.last=this.first;
+//         this.first=temp;
+//         console.log(this.first+" "+this.last);
+//     }
+// }
+
+
+// var player1=new user("nibraz","rehaman",21);
+// var player2=new user("virat","kohli",33);
+
+// player1.getFullName();
+// player2.getFullName();
+// player2.reverse();
+
+
+
+
+
+
+
+// var n='NIBRAZ'
+// console.log(n.toLowerCase());
+
+
+
+
+
